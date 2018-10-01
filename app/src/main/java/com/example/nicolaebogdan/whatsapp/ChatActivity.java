216cc982376929c40525cab96578627f6d2a2ea6
Nicolae Bogdan <bogdan.nicolae@teamextension.ro>
2018-10-01 14:13:12 +0100

    add chat one to one functionality

--- app/src/main/java/com/example/nicolaebogdan/whatsapp/ChatActivity.java
+++ app/src/main/java/com/example/nicolaebogdan/whatsapp/ChatActivity.java
@@ -1,13 +1,185 @@
 package com.example.nicolaebogdan.whatsapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private String messageReceiverID,messageReceiverName,messageReceiverImage,messageSenderID;
+    private TextView userName,userLastSeen;
+    private CircleImageView userImage;
+    private Toolbar ChatToolbar;
+    private ScrollView messagesScrollView;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+
+    private ImageButton SendMessageButton;
+    private EditText MessageInputText;
+
+    private List<Messages> messagesList = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private MessageAdapter messageAdapter;
+    private RecyclerView userMessagesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mAuth = FirebaseAuth.getInstance();
+        messageSenderID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        messageReceiverID = getIntent().getExtras().get("visit_user_id").toString();
+        messageReceiverName = getIntent().getExtras().get("visit_user_name").toString();
+        messageReceiverImage = getIntent().getExtras().get("visit_user_image").toString();
+
+        InitializeControllers();
+
+        userName.setText(messageReceiverName);
+        Picasso.get().load(messageReceiverImage).placeholder(R.drawable.profile_image).into(userImage);
+
+        SendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendMessage();
+            }
+        });
+    }
+
+    private void InitializeControllers() {
+
+
+        ChatToolbar = findViewById(R.id.chat_toolbar);
+        setSupportActionBar(ChatToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionBarView = layoutInflater.inflate(R.layout.custom_chat_bar,null);
+        actionBar.setCustomView(actionBarView);
+
+        userImage = findViewById(R.id.custom_profile_image);
+        userName = findViewById(R.id.custom_profile_name);
+        userLastSeen = findViewById(R.id.custom_user_last_seen);
+        SendMessageButton = findViewById(R.id.send_message_btn);
+        MessageInputText = findViewById(R.id.input_message);
+        messagesScrollView = findViewById(R.id.my_scroll_view_chat_one_to_one);
+
+        messageAdapter = new MessageAdapter(messagesList);
+        userMessagesList = findViewById(R.id.private_messages_list_of_users);
+        linearLayoutManager = new LinearLayoutManager(this);
+        userMessagesList.setLayoutManager(linearLayoutManager);
+        userMessagesList.setAdapter(messageAdapter);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        RootRef.child("Messages").child(messageSenderID).child(messageReceiverID).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Messages messages = dataSnapshot.getValue(Messages.class);
+                messagesList.add(messages);
+                messageAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void SendMessage(){
+
+        String messageText = MessageInputText.getText().toString();
+
+        if(TextUtils.isEmpty(messageText)){
+            Toast.makeText(ChatActivity.this,"first write your message...",Toast.LENGTH_SHORT).show();
+        }else{
+            String messageSenderRef = "Messages/" + messageSenderID + "/" + messageReceiverID;
+            String messageReceiverRef = "Messages/" + messageReceiverID + "/" + messageSenderID;
+
+            DatabaseReference userMessageKeyRef = RootRef.child("Messages")
+                    .child(messageSenderID).child(messageReceiverID).push();
+
+            String messagePushID = userMessageKeyRef.getKey();
+
+            Map messageTextBody = new HashMap();
+            messageTextBody.put("message",messageText);
+            messageTextBody.put("type","text");
+            messageTextBody.put("from",messageSenderID);
+
+            Map messageBodyDetails = new HashMap();
+            messageBodyDetails.put(messageSenderRef + "/" + messagePushID,messageTextBody);
+            messageBodyDetails.put(messageReceiverRef + "/" + messagePushID,messageTextBody);
+
+            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(ChatActivity.this,"Message Sent Successfully...",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(ChatActivity.this,"Error",Toast.LENGTH_SHORT).show();
+                    }
+                    MessageInputText.setText("");
+                }
+            });
+
+        }
+
     }
 }
